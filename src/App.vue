3bf117e5977f53b5a9ff974ee28ca4751df30f1f
DESKTOP-RI27DJH\Lenovo <146740721@qq.com>
2025-09-03 18:37:03 +0800

    feat:change lable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to My CICD Project Vue.js App"/>
   </div>
 </template>
 
