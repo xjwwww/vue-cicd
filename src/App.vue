f49a4806ef2e4a447152f3ed5fcb8ccac70e7953
xjw <1146740721@qq.com>
2025-09-09 15:00:18 +0000

    update src/App.vue.
    Signed-off-by: xjw <1146740721@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My CICD Project 2025 4 Vue.js App"/>
+    <HelloWorld msg="Welcome to My CICD Project 2025 1 Vue.js App"/>
   </div>
 </template>
 
